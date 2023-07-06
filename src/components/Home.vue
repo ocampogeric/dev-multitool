dfc8919e359c0fc8d1b140a11d6473b14daae31c
ocampogeric <ocampogeric@gmail.com>
2023-07-05 23:00:06 -0600

    Added home message

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -9,8 +9,23 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <v-container>
+    <v-row justify="center">
+      <v-col cols="12">
+        <h1 class="text-center text-grey-darken-3">Welcome to Dev Multitool</h1>
+      </v-col>
+      <v-col cols="6">
+        <p class="text-center">Work in progress...</p>
 
+        <p class="text-center">
+          You can try: 
+          <v-btn :to="{name: 'base64tool'}" variant="plain" class="text-decoration-underline">
+            B64 Tools
+          </v-btn>
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
